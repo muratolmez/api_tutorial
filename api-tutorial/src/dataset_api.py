4f91ca5150ae4bd2076f7b73fb47e4a1efa6db20
Murat Olmez <Mesut@Monster>
2017-03-13 14:00:15 +0200

    13.03.2017

--- api-tutorial/src/dataset_api.py
+++ api-tutorial/src/dataset_api.py
@@ -2,9 +2,13 @@ import endpoints
 from protorpc import messages, message_types
 from protorpc import remote
 from google.appengine.ext import ndb
+from google.appengine.datastore.datastore_v4_pb import GqlQuery
+import logging
+from google.appengine._internal.django.core.mail.backends import console
 
 
-
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 class User(messages.Message):
     user_auth_id = messages.StringField(1)
@@ -15,10 +19,14 @@ class User(messages.Message):
     user_gender = messages.StringField(6,required = True)
     user_phone = messages.IntegerField(7,required = True)
     user_birthday = messages.StringField(8,required = True)
-    user_created_date=message_types.DateTimeField(9)
+    user_created_date=messages.IntegerField(9)
     user_rank = messages.IntegerField(10)
     
-    
+  
+class aluser(messages.Message):
+    auth_id = messages.StringField(1)
+  
+  
     
         
 class UserModel(ndb.Model):
@@ -30,21 +38,25 @@ class UserModel(ndb.Model):
     user_gender = ndb.StringProperty(required = True)
     user_phone = ndb.IntegerProperty(required = True)
     user_birthday = ndb.StringProperty(required = True)
-    user_created_date=ndb.DateTimeProperty(required = True)
+    user_created_date=ndb.IntegerProperty(required = True)
     user_rank=ndb.IntegerProperty()
 
 class UserList(messages.Message):
     users = messages.MessageField(User,1,repeated = True)
         
+
+
+
 @endpoints.api(name = 'userProfile', version='vGDL',
                description='API For User')
 
 class User_Api(remote.Service):
+
     @endpoints.method(User,User,
                       name='user.insert',
                       path='user_insert',
                       http_method= 'POST')
-    
+     
     def user_insert(self,request):
         UserModel(user_auth_id= request.user_auth_id,
                   user_name= request.user_name,
@@ -77,5 +89,30 @@ class User_Api(remote.Service):
         
         return UserList(users=users_list)
         
+    
+    @endpoints.method(aluser,User,
+        name='user.al',
+        path= 'user_al',
+        http_method='POST'
+                     )
+    def user_json(self,request):
+        
+        q = UserModel.gql("WHERE user_auth_id = '12'")
+        for user in q:
+            #print user.user_auth_id
+
+            usera = User(user.user_auth_id,
+                         user.user_name,
+                         user.user_surname,
+                         user.user_email,
+                         user.user_city, 
+                         user.user_gender,
+                         user.user_phone,
+                         user.user_birthday,
+                         user.user_created_date,
+                         user.user_rank) 
+        
+        return usera
+
 
 api = endpoints.api_server([User_Api])
